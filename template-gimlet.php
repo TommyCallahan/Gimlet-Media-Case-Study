13586ad2abc925b59ffa524fd30a94f0820e3329
TommyCallahan <tom@designcommission.com>
2017-02-05 23:17:04 -0800

    initial push

---
+++ template-gimlet.php
@@ -0,0 +1,121 @@
+<?php 
+/* Template Name: Gimlet */
+get_header();
+?>
+
+
+
+<div class="CaseStudyHero gimlet">
+    <div class="CaseStudyHeroOverlay">
+        <h2>Gimlet Media</h2>
+        <p>Gimlet Media is a digital media company and podcast network, focused on producing high quality narrative podcasts.
+        </p>
+    </div>
+</div>
+
+<div class="CaseStudyIntro container">
+    <p class="CaseStudyTextBlock">Gimlet Media was born from the popular podcast StartUp, hosted by former This American Life and Planet Money producer, Alex Blumberg. His goal was to start a new podcast network company that would seize on the opportunity presented by the rise in popularity of podcasts. As they began to mature it was clear they needed a team to provide clear leadership on their website and digital presence.</p>
+</div>
+
+<h2 class="CaseStudyHeaderGimlet blue-text">Growth of the Gimlet Media Network</h2>
+
+<figure class="gimlet-align container CaseStudyImageGimlet">
+    <img alt="" class="CaseStudyImageGimlet" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/gimlet-timeline.png">
+</figure>
+
+<div class="CaseStudyTextBlock blue-bg CaseStudyPadding lrg-padding-bottom margin-top-4">
+    <h2 class="CaseStudyHeaderGimlet">Concept Driven Design &amp; Development</h2>
+    <p class="container">Our first order of business with Gimlet’s website was to provide a conceptual direction for the website based on the company’s brand. Without a lot of source material to start with, we began to create a visual system that would serve as a general guideline and was helpful in building consensus with the client’s team.</p>
+    <div class="GimletLogoBG"></div>
+</div>
+
+<div class="gimletbg">
+    <div class="container">
+        <div class="cs-chrome">
+            <div class="blue-bar"></div><img alt="" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/replyg2.jpg">
+        </div>
+        
+        <div class="cs-chrome" style="margin-top:20rem">
+            <div class="blue-bar-horizontal"></div>
+            <div class="blue-bar blue-bar-snap"></div>
+            <div style="line-height:27px;" class="steps">
+                Ideate &amp;<br> Develop
+            </div>
+            <div class="blue-bar"></div><img alt="" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/replyg1.jpg">
+            <div class="blue-bar"></div>
+        </div>
+    </div>
+</div>
+
+<div class="CaseStudyTextBlock blue-bg CaseStudyPadding">
+    <h2 class="CaseStudyHeaderGimlet">A Place for Everything</h2>
+    <p class="container lrg-padding-bottom">We believe that good information design is critical to user experience on the web so we dedicated time to refining how the site’s content was put together and what the best navigation system was.</p>
+   
+    <div class="CaseStudyTextBlock"></div>
+    <figure class="gimlet-align container CaseStudyImageGimlet">
+        <img alt="" class="CaseStudyImageGimlet" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/nav-graphic.gif">
+    </figure>
+   
+    <div class="CaseStudyTextBlock blue-bg CaseStudyPadding lrg-padding-bottom"></div>
+    
+    <p class="container">With branding guidelines in place and a new architecture, we then set about designing the interface for both the desktop and mobile views of the site. The goal was to create a simple presentation of information that didn’t require the client investing in a lot of new visual content to be created from scratch.</p>
+    
+    <figure class="CaseStudyFigure gimlet-align margin-btm-4rem padding-btm-4rem">
+        <img alt="" class="CaseStudyImageGimlet" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/gimletresponsive.png">
+    </figure>
+</div>
+
+<div class="CaseStudyTextBlock CaseStudyPadding lrg-padding-bottom">
+    <h2 class="CaseStudyHeaderGimlet blue-text">The Art of Managing Content</h2>
+    <p class="CaseStudyTextBlock container">While we iterated on typography, colors, and other visual aspects we began the work of setting up the back-end for the site in Wordpress and migrating content from the first version of the site. This involved writing a lot of custom API connects with services like MailChimp and SoundCloud as well as customization of the WordPress administrative experience so that it was easily for producers to maintain and update.</p>
+    <div class="opacitybox container">
+      <img alt="" class="CaseStudyImageGimlet" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/mailchimp.png">
+      <img alt="" class="CaseStudyImageGimlet" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/soundcloud.png">
+      <img alt="" class="CaseStudyImageGimlet" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/wordpress.png">
+    </div>
+</div>
+
+<div class="CaseStudyTextBlock blue-bg CaseStudyPadding">
+    <h2 class="CaseStudyHeaderGimlet">Social. Sharable. Memorable.</h2>
+    <p class="container lrg-padding-bottom">Social media is one of the key factors in attracting new listeners for Gimlet, but the standard content feeds most users see when logging in to Twitter or Facebook are filled with posts from a huge variety of sources. Being able to stand out is difficult and important. By building a custom plugin to create images from episode quotes helps the tweets and posts stand out and makes for more compelling, sharable content.</p>
+    
+    <div style="font-size:0;" class="CaseStudyTextBlock container gimlet-align">
+        <img alt="" class="CaseStudyImageGimlet br15" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/cardgif.gif">
+        <div class="col colm2"><img alt="" class="CaseStudyImageGimlet br15" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/twittercard.gif">
+        </div>
+        <div class="col"><img alt="" class="CaseStudyImageGimlet br15" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/fbcard.gif">
+        </div>
+    </div>
+
+    <div class="CaseStudyTextBlock pb15"><img alt="" class="bow" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/bow.png"></div>
+</div>
+
+<div class="CaseStudyBlock">
+    <h2 class="CaseStudyHeaderGimlet blue-text">Membership Has Its Benefits</h2>
+</div>
+
+<p class="CaseStudyTextBlock container">One of these releases included the launch of Gimlet’s membership program. Designing for an experience that was simple but compelling, we worked with Gimlet’s team to identify messaging and content for the landing pages and refining the rewards offered to paying members.</p>
+
+<div class="blue-box CaseStudyTextBlock grey-border"><img alt="" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/gimlet-screen-2.jpg" style="width:100%; max-width:830px;"></div>
+
+<p class="CaseStudyTextBlock margin-top-4 container">Building on the integration of Memberful with Wordpress we were able to design, build and launch a highly-successful membership program for Gimlet in a matter of weeks.</p>
+
+
+<div class="awards-section"></div>
+<div class="Testimonial container">
+    <div class="TestimonialImage"><img alt="Matt Lieber, Co-Founder, Gimlet Media" class="TestimonialHeadshot" src="<?php bloginfo('url'); ?>/wp-content/themes/dc-theme/img/gimlet-matt.png"></div>
+   
+    <div class="TestimonialRight">
+        <h3 class="TestimonialHeader"></h3>
+        <p class="TestimonialCopy">“Design Commission is easy to work with, responsive, and very professional. They put in long hours on the week of our product launch, and we could tell they really cared. They are also flexible, which is helpful for startups like ours, where new things come up sometimes unpredictably and with immediate deadlines.”</p>
+        <p class="TestimonialAttribution">Matt Lieber, Co-Founder, Gimlet Media</p>
+        <div style="display:none; margin:0 auto; width:340px; -webkit-border-radius: 15px 15px 15px 15px; border-radius: 15px 15px 15px 15px; background-color:#ebebeb;padding:30px 0px 50px 20px;">
+            <iframe align="center" frameborder="0" height="570" src="https://marvelapp.com/d96gc7?emb=1" style="border:1px solid #d0cfcf;" width="300"></iframe>
+        </div>
+    </div>
+    
+</div>
+
+<a class="NextControl" href="/case-studies/haikudeck/"><span>View Next Project</span></a>
+
+<?php get_footer(); ?>
